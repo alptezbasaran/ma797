a2e75ff39f59fa3f20acf5c628f97b9a52881afa
Alp <alptezbasaran@gmail.com>
2019-09-07 17:25:25 -0400

    progress on q1

--- Assignments/A1/q1.py
+++ Assignments/A1/q1.py
@@ -6,23 +6,19 @@ Created on Sat Sep  7 14:58:44 2019
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
-from sklearn import mixture
-
 
 def generate_gauss_classes(m,S,P,N):
   X = np.zeros((m.shape[1],N))
+  y = np.zeros(N)
   print(X.shape)
   for j in range(0,m.shape[1]):
     size = int(np.fix(P[j]*N))
-    new_array = np.random.multivariate_normal(m[j], S, size).T
-    print('ayak')
-    print(new_array)
-    X[:, j*size: (j+1)*size] = new_array
-  print('ayak yalamak')
-  print(X)
-  return(X)
+    new_samples = np.random.multivariate_normal(m[j], S, size).T
+    X[:, j*size: (j+1)*size] = new_samples
+    y[j*size: (j+1)*size] = j+1
+
+  print(y)
+  return(X, y)
 
 # Constant seed
 np.random.seed(0)
@@ -35,4 +31,4 @@ S = 0.8 * np.eye(3)
 # Probabilities
 P = np.array([1/3, 1/3, 1/3])
 # Random data from multivariate_normal
-X = generate_gauss_classes(m, S, P, N_samples)
+[X, y] = generate_gauss_classes(m, S, P, N_samples)
